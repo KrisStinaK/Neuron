a84d9ae85fcb8e404de90682ec6adfe5e14a79e1
Kristina <krasilnikova13570@gmail.com>
2026-02-03 18:58:46 +0300

    Добавила модели

--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = (
+        ('student', 'Студент'),
+        ('teacher', 'Преподаватель'),
+        ('admin', 'Администратор'),
+    )
+
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='student')
+    bio = models.TextField(blank=True)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+
+    def __str__(self):
+        return self.username
